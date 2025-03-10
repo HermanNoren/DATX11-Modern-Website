d5997ae99c380795555f2fe1d4ba213edf32ea19
hildalange <hilda.lange@gmail.com>
2025-03-10 11:17:21 +0100

    z-value

--- src/app/_components/LoadingScreen.tsx
+++ src/app/_components/LoadingScreen.tsx
@@ -1,40 +1,54 @@
 "use client";
 import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function LoadingScreen({ onFinish }: { onFinish: () => void }) {
-  const [fadeOut, setFadeOut] = useState(false);
+  const [exit, setExit] = useState(false);
+  const [isActive, setIsActive] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setFadeOut(true), 3000);
-    setTimeout(onFinish, 1000);
+    setTimeout(() => setExit(true), 3000);
+    setTimeout(onFinish, 4500);
   }, [onFinish]);
 
-  return (
-    <div className={`fixed inset-0 flex items-center justify-center bg-white z-50 transition-opacity duration-1000 ${
-        fadeOut ? "opacity-0" : "opacity-100" }`}>
+  const clipPathVariants = {
+    initial: {
+      clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+    },
+    exit: {
+      clipPath: "polygon(100% 0, 100% 0, 100% 100%, 100% 100%)",
+      transition: { duration: 1.5, ease: [0.76, 0, 0.24, 1] },
+    },
+  };
 
+  return (
+    <motion.div
+      className="fixed inset-0 flex items-center justify-center bg-white z-1000"
+      variants={clipPathVariants}
+      initial="initial"
+      animate={exit ? "exit" : "initial"}
+    >
       <div className="flex items-center">
         <p className="text-4xl text-gray-1000 font-bold mr-4">Loading</p>
-        <div className="w-8 h-8 border-3 border-t-3 border-gray-700 border-solid rounded-full animate-spin"
-          style={{ borderTopColor: "transparent" }}>
-        </div>
+        <div
+          className="w-8 h-8 border-3 border-t-3 border-gray-700 border-solid rounded-full animate-spin"
+          style={{ borderTopColor: "transparent" }}
+        ></div>
       </div>
-
-      <p className={`absolute top-0 right-0 p-4 text-2xl text-gray-700 transition-opacity duration-1000 ${
-          fadeOut ? "opacity-0" : "opacity-100" }`}> CUBE/KUB/CUBO/KUUTIO/立方体
+      
+      <p className="absolute top-0 right-0 p-4 text-2xl text-gray-700">
+        CUBE/KUB/CUBO/KUUTIO/立方体
+      </p>
+      <p className="absolute top-1/4 rotate-[90deg] p-1 ml-225 text-2xl font-bold text-gray-700">
+        CUBE/KUB/CUBO/KUUTIO/立方体
+      </p>
+      <p className="absolute bottom-0 left-0 p-4 text-2xl text-gray-700">
+        CUBE/KUB/CUBO/KUUTIO/立方体
+      </p>
+      <p className="absolute bottom-1/4 rotate-[270deg] p-1 mr-225 text-2xl font-bold text-gray-700">
+        CUBE/KUB/CUBO/KUUTIO/立方体
       </p>
 
-      <p className={`absolute top-1/4 rotate-[90deg] p-1 ml-225 text-2xl font-bold text-gray-700 transition-opacity duration-1000 ${
-          fadeOut ? "opacity-0" : "opacity-100" }`}> CUBE/KUB/CUBO/KUUTIO/立方体
-      </p>
-
-      <p className={`absolute bottom-0 left-0 p-4 text-2xl text-gray-700 transition-opacity duration-1000 ${
-          fadeOut ? "opacity-0" : "opacity-100" }`}> CUBE/KUB/CUBO/KUUTIO/立方体
-      </p>
-
-      <p className={`absolute bottom-1/4 rotate-[270deg] p-1 mr-225 text-2xl font-bold text-gray-700 transition-opacity duration-1000 ${
-          fadeOut ? "opacity-0" : "opacity-100" }`}> CUBE/KUB/CUBO/KUUTIO/立方体
-      </p>
-    </div>
+    </motion.div>
   );
 }
