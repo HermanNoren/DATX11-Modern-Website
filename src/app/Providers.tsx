df7438fc4a78a174012a31bf49a02ef82bec39ed
KingHermando <herman.noren@outlook.com>
2025-03-18 19:29:05 +0100

    newMenu

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -7,7 +7,7 @@ export default function Providers(props: { children: React.ReactNode }) {
   const [isLoading, setIsLoading] = useState(true);
   return (
     <main className={isLoading ? "w-screen h-screen overflow-hidden" : ""}>
-      <LoadingScreen onFinish={() => setIsLoading(false)} />
+      {/*<LoadingScreen onFinish={() => setIsLoading(false)} />*/}
       {props.children}
     </main>
   );
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -16,6 +16,7 @@ import { usePathname } from "next/navigation";
 import gsap from "gsap";
 import { cn } from "@/utils/cn";
 import TransitionLink from "./TransitionLink";
+import VelocityScroll from "../VelocityScroll";
 
 type link = {
   name: string;
@@ -70,32 +71,35 @@ export default function Header() {
   );
 }
 
-const menuClipPath = {
+const menuAnim = {
   initial: {
-    clipPath: "polygon(100% 0, 100% 0, 100% 100%, 100% 100%)",
-  },
-  enter: {
-    clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-    transition: { duration: 1.5, ease: [0.76, 0, 0.24, 1] },
-  },
-  exit: {
-    clipPath: "polygon(100% 0, 100% 0, 100% 100%, 100% 100%)",
-    transition: { duration: 1.5, ease: [0.76, 0, 0.24, 1] },
-  },
-};
-
-const menuMoveContent = {
-  initial: {
-    x: "10em",
+    x: "100%",
   },
   enter: {
     x: "0",
-    transition: { duration: 1.5, ease: [0.76, 0, 0.24, 1] },
+    transition: { duration: 1, ease: [0.76, 0, 0.24, 1] },
   },
   exit: {
-    x: "10em",
-    transition: { duration: 1.5, ease: [0.76, 0, 0.24, 1] },
+    x: "100%",
+    transition: { duration: 1, ease: [0.76, 0, 0.24, 1] },
   },
+};
+
+export const menuLinkSlide = {
+  initial: {
+    x: "80px",
+    opacity: 0.5,
+  },
+  enter: (i: number) => ({
+    x: "0px",
+    opacity: 1,
+    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.05 * i },
+  }),
+  exit: (i: number) => ({
+    x: "80px",
+    opacity: 0.5,
+    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.05 * i },
+  }),
 };
 
 function FullScreenMenu(props: {
@@ -104,7 +108,6 @@ function FullScreenMenu(props: {
   links: link[];
 }) {
   const menu = useRef<HTMLDivElement>(null);
-  const [isAnimating, setIsAnimating] = useState<boolean>(true);
   const cubeLangs = "CUBE/KUB/CUBO/KUUTIO/立方体";
   const pathname = usePathname();
 
@@ -113,13 +116,6 @@ function FullScreenMenu(props: {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   const linkRefs = useRef(props.links.map(() => createRef<HTMLDivElement>()));
-
-  useEffect(() => {
-    if (!props.isActive) setIsAnimating(true);
-    setTimeout(() => {
-      setIsAnimating(false);
-    }, 1500);
-  }, [props.isActive]);
 
   function onClick() {
     props.setIsActive(false);
@@ -156,57 +152,62 @@ function FullScreenMenu(props: {
   return (
     <motion.div
       ref={menu}
-      variants={menuClipPath}
+      variants={menuAnim}
       initial="initial"
       animate="enter"
       exit="exit"
-      className="absolute inset-0 w-screen h-lvh z-[99] bg-neutral-200"
+      className="absolute top-0 right-0 w-[30em] h-lvh z-[99] bg-dark-background"
     >
-      {isAnimating ? <div className="absolute inset-0 z-[100]"></div> : null}
-      <motion.div
-        variants={menuMoveContent}
-        initial="initial"
-        animate="enter"
-        exit="exit"
-        className="w-full h-full"
-      >
-        <span className="absolute text-4xl rotate-z-270 origin-top-left bottom-[-1em]">
-          {cubeLangs}/{cubeLangs}
-        </span>
-        <nav className="relative w-full h-full grid place-items-center">
+      <div className="w-full h-full flex flex-col justify-evenly">
+        <div className="container">
+          <p className="uppercase pb-4">Menu</p>
+          <div className="w-full h-[1px] bg-foreground opacity-50"></div>
+        </div>
+        <nav className="relative grid place-items-center">
           <ul className="flex flex-col gap-1 uppercase">
             {props.links.map((link, i) => {
               const isActive = link.href === pathname;
               return (
-                <div
+                <motion.div
                   key={i}
+                  variants={menuLinkSlide}
+                  custom={i}
+                  initial="initial"
+                  animate="enter"
+                  exit="exit"
                   onMouseEnter={() => onMouseEnter(i)}
                   onMouseLeave={onMouseLeave}
-                  className="relative w-fit"
+                  className="relative w-fit flex gap-6"
                 >
-                  <div
-                    ref={linkRefs.current[i]}
-                    className={cn(
-                      "absolute left-[-0.75em] top-[0.32em] text-5xl will-change-transform",
-                      isActive ? "scale-100" : "scale-0"
-                    )}
-                  >
-                    •
+                  <div className="relative">
+                    <div
+                      className={cn(
+                        "relative text-4xl will-change-transform pt-[0.14em]",
+                        isActive ? "scale-100" : "scale-0"
+                      )}
+                      ref={linkRefs.current[i]}
+                    >
+                      •
+                    </div>
                   </div>
+
                   <TransitionLink href={link.href}>
                     <Button
                       key={i}
                       text={link.name}
                       onClick={onClick}
-                      className="text-7xl uppercase"
+                      className="text-5xl uppercase"
                     />
                   </TransitionLink>
-                </div>
+                </motion.div>
               );
             })}
           </ul>
         </nav>
-      </motion.div>
+        <VelocityScroll className={"text-4xl"}>
+          {cubeLangs}/{cubeLangs}/
+        </VelocityScroll>
+      </div>
     </motion.div>
   );
 }
