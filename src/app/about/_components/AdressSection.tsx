e0483f8b13ece383d0a0f2ab73f675016692a4f8
jennycarlsson <44492196+jennycarlsson@users.noreply.github.com>
2025-03-24 22:10:45 +0100

    imroved sizing of elements

--- src/app/about/_components/AdressSection.tsx
+++ src/app/about/_components/AdressSection.tsx
@@ -10,32 +10,32 @@ export default function ContactSection() {
       <div className="w-full max-w-[1200px] text-center flex flex-col items-center relative">
         
         {/* Positioned text behind the cube */}
-        <h2 className="text-6xl font-light absolute top-[-15%] z-0">ANY</h2>
-        <h2 className="text-6xl font-light absolute top-[10%] z-0">QUESTIONS?</h2>
+        <h2 className="text-7xl font-light absolute top-[-30%] z-0">ANY</h2>
+        <h2 className="text-7xl font-light absolute top-[-5%] z-0">QUESTIONS?</h2>
         
         {/* Cube positioned in the center */}
-        <div className="w-64 h-64 relative z-10">
+        <div className="w-80 h-80 relative z-10">
           <Canvas>
-            <Cube size={2.5} />
+            <Cube size={3.3} />
             <Environment /*files="/skybox.exr"*/ preset="studio" />
           </Canvas>
         </div>
         
         {/* Positioned text in front of the cube */}
-        <h2 className="text-6xl font-light mt-[-50px] z-20 relative">CONTACT US</h2>
+        <h2 className="text-7xl font-light mt-[-80px] z-20 relative">CONTACT US</h2>
         
         {/* Side text fully aligned to the edges */}
-        <div className="absolute left-0 top-1/3 transform -translate-y-1/2 rotate-[-90deg] origin-left text-sm text-right pl-4">
+        <div className="absolute left-0 top-1/3 transform -translate-y-1/2 rotate-[-90deg] origin-left text-s text-right pl-4">
           CONTACT@DECUBE.COM
         </div>
-        <div className="absolute left-0 top-100 rotate-[-90deg] origin-left text-sm text-right pl-4">
+        <div className="absolute left-0 top-100 rotate-[-90deg] origin-left text-s text-right pl-4">
           +46 123 456 789
         </div>
-        <div className="absolute right-0 top-1/3 transform rotate-[90deg] origin-right text-sm text-left pr-4">
+        <div className="absolute right-0 top-1/3 transform rotate-[90deg] origin-right text-s text-left pr-4">
           DECUBE STREET,<br />
           GOTHENBURG, SWEDEN
         </div>
-        <div className="absolute right-0 top-100 transform rotate-[90deg] origin-right text-sm text-left pr-4">
+        <div className="absolute right-0 top-100 transform rotate-[90deg] origin-right text-s text-left pr-4">
           WORLDWIDE SHIPPING &<br />
           NO RETURNS
         </div>
