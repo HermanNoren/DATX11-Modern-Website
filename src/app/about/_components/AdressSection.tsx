425453dffa1dc95cbd041baedd37ef317d6912fb
KingHermando <herman.noren@outlook.com>
2025-03-24 23:33:12 +0100

    QOL fixes

--- src/app/about/_components/AdressSection.tsx
+++ src/app/about/_components/AdressSection.tsx
@@ -6,13 +6,58 @@ import { Environment } from "@react-three/drei";
 
 export default function ContactSection() {
   return (
-    <section className="w-screen flex justify-center items-center relative py-48 pb-150">
-      <div className="w-full max-w-[1200px] text-center flex flex-col items-center relative">
-        
+    <section className="relative z-20 w-screen h-screen bg-white">
+      <div className="container w-full h-full grid place-items-center">
+        <div className="flex flex-col justify-between h-[30em]">
+          <h2 className="text-7xl text-center z-0">
+            ANY <br /> QUESTIONS?
+          </h2>
+          <h2 className="text-7xl font-light z-20 relative pb-6">CONTACT US</h2>
+        </div>
+        <div className="absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] w-80 h-80 z-10">
+          <Canvas>
+            <Cube size={3.3} />
+            <Environment /*files="/skybox.exr"*/ preset="studio" />
+          </Canvas>
+        </div>
+      </div>
+      <div className="absolute top-0 left-0 w-full h-full container grid grid-cols-1 grid-rows-2 gap-36">
+        <div className="w-full h-full flex justify-between items-end">
+          <div className="w-fit h-fit rotate-[-90deg] origin-top-left">
+            <p className="">CONTACT@DECUBE.COM</p>
+          </div>
+          <div className="w-fit h-fit rotate-[90deg] origin-top-right ">
+            <p className="">
+              DECUBE STREET,
+              <br />
+              GOTHENBURG, SWEDEN
+            </p>
+          </div>
+        </div>
+        <div className="w-full h-full flex justify-between">
+          <div className="w-fit h-fit rotate-[-90deg] origin-top-left">
+            <p className="translate-x-[-100%]">+46 123 456 789</p>
+          </div>
+          <div className="w-fit h-fit rotate-[90deg] origin-top-right ">
+            <p className="translate-x-[100%]">
+              WORLDWIDE SHIPPING &<br />
+              NO RETURNS
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function ContactSection2() {
+  return (
+    <section className="w-screen h-screen flex justify-center items-center relative py-48 pb-150">
+      <div className="container w-full h-full text-center flex flex-col items-center relative border">
         {/* Positioned text behind the cube */}
-        <h2 className="text-7xl font-light absolute top-[-30%] z-0">ANY</h2>
-        <h2 className="text-7xl font-light absolute top-[-5%] z-0">QUESTIONS?</h2>
-        
+        <h2 className="text-7xl font-light absolute top-[-29%] z-0">
+          ANY <br /> QUESTIONS?
+        </h2>
         {/* Cube positioned in the center */}
         <div className="w-80 h-80 relative z-10">
           <Canvas>
@@ -20,25 +65,28 @@ export default function ContactSection() {
             <Environment /*files="/skybox.exr"*/ preset="studio" />
           </Canvas>
         </div>
-        
         {/* Positioned text in front of the cube */}
-        <h2 className="text-7xl font-light mt-[-80px] z-20 relative">CONTACT US</h2>
-        
-        {/* Side text fully aligned to the edges */}
-        <div className="absolute left-0 top-1/3 transform -translate-y-1/2 rotate-[-90deg] origin-left text-s text-right pl-4">
+        <h2 className="text-7xl font-light mt-[-80px] z-20 relative">
+          CONTACT US
+        </h2>
+        {/*
+        <div className="absolute left-[3em] top-1/3 transform -translate-y-1/2 rotate-[-90deg] origin-left text-s text-right">
           CONTACT@DECUBE.COM
         </div>
-        <div className="absolute left-0 top-100 rotate-[-90deg] origin-left text-s text-right pl-4">
+        <div className="absolute left-[3em] top-100 rotate-[-90deg] origin-left text-s text-right">
           +46 123 456 789
         </div>
-        <div className="absolute right-0 top-1/3 transform rotate-[90deg] origin-right text-s text-left pr-4">
-          DECUBE STREET,<br />
+        <div className="absolute right-[3.6em] top-1/3 transform rotate-[90deg] origin-right text-s text-left">
+          DECUBE STREET,
+          <br />
           GOTHENBURG, SWEDEN
         </div>
-        <div className="absolute right-0 top-100 transform rotate-[90deg] origin-right text-s text-left pr-4">
+        <div className="absolute right-[3.6em] top-100 transform rotate-[90deg] origin-right text-s text-left">
           WORLDWIDE SHIPPING &<br />
           NO RETURNS
         </div>
+        */}
+        {/* Side text fully aligned to the edges */}
       </div>
     </section>
   );
--- src/app/about/_components/videoSection.tsx
+++ src/app/about/_components/videoSection.tsx
@@ -47,6 +47,7 @@ export default function VideoSection() {
         >
           <MaskText
             stagger={0.002}
+            delay={0.7}
             phrase="OUR TEAM IS OBSESSED WITH MINIMALISM, SYMMETRY, AND MATERIAL INNOVATION—PUSHING THE BOUNDARIES OF DESIGN TO DELIVER PIECES THAT EMBODY SOPHISTICATION AND EXCLUSIVITY."
           />
         </p>
@@ -77,6 +78,7 @@ export default function VideoSection() {
         >
           <MaskText
             stagger={0.002}
+            delay={0.7}
             phrase="TO REFLECT ITS EXCEPTIONAL CRAFTSMANSHIP, EACH DECUBE ARRIVES IN BESPOKE PACKAGING WITH A CERTIFICATE OF AUTHENTICITY—A MARK OF ITS RARITY AND PRESTIGE."
           />
         </p>
