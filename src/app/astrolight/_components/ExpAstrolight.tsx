307d886a75a01a8b6606134af0ca8d43fe88a5ea
jennycarlsson <44492196+jennycarlsson@users.noreply.github.com>
2025-03-24 23:50:52 +0100

    minor fix

--- src/app/astrolight/_components/ExpAstrolight.tsx
+++ src/app/astrolight/_components/ExpAstrolight.tsx
@@ -23,7 +23,7 @@ export default function ExpAstrolight() {
       >
         <span>EXPERIENCE</span>
         {/* Small Video Box */}
-        <div className="w-[2em] h-[0.5em] md:w-[1.2em] md:h-[0.8em] rounded-xl shadow-md overflow-hidden flex items-center">
+        <div className="w-[2em] h-[0.5em] md:w-[1.2em] md:h-[0.8em] shadow-md overflow-hidden flex items-center">
           <video
             className="w-full h-full object-cover"
             autoPlay
