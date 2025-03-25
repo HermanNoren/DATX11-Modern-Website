63963c063e509fb67f58ac77e7775f531cc18ff9
KingHermando <herman.noren@outlook.com>
2025-03-25 23:36:20 +0100

    Fix header alignment on about page

--- src/app/about/_components/aboutTitle.tsx
+++ src/app/about/_components/aboutTitle.tsx
@@ -2,7 +2,7 @@ import MaskText from "@/components/MaskText";
 
 export default function AboutTitle() {
   return (
-    <h1 className="text-9xl md:text-5xl lg:text-9xl font-light text-center mt-3 mb-0">
+    <h1 className="text-9xl md:text-5xl lg:text-9xl font-light text-center pt-3 mb-0">
       <MaskText phrase="About deCube" delay={0.7} />
     </h1>
   );
